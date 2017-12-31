330def2f40751125b325eb67c708916ec2a645fc
=shivam dwivedi <=shivamd20@gmail.com>
2018-01-01 01:49:40 +0530

    modularizing code

---
+++ microservices/real/app/src/liveQuery.js
@@ -0,0 +1,74 @@
+var fs = require('fs');
+var path = require('path');
+var LivePg = require('pg-live-select');
+
+
+// // Load the SELECT query from an external file
+// var QUERY = "select * from ramu";
+
+
+// // Create a live select instance
+
+// try{
+// liveDb.select(QUERY)
+//   .on('update', function(diff, data) {
+//     // Handle the changes here...
+//     console.log(diff, data);
+//   });
+// }catch(e){
+// console.log(JSON.stringify(e));
+// }
+
+// // On Ctrl+C, remove triggers and exit
+
+
+class LiveQuery{
+
+    getConnectionString(){
+            return 'postgres://'+this.dbcred.username+':'
+            +this.dbcred.password
+            +'@'+this.dbcred.hostname+':'+this.dbcred.port+'/'+this.dbcred.dbname;
+    }
+
+    constructor(value){
+         if(!process.argv){
+            this.dbcred={
+              username:process.env.POSTGRES_USERNAME,
+              password:process.env.POSTGRES_PASSWORD,
+              hostname:process.env.POSTGRES_HOSTNAME,
+              port:process.env.POSTGRES_PORT,
+              dbname:'hasuradb'
+            }
+            }
+            else{
+              this.dbcred={
+                username:process.argv.uname,
+                password:process.argv.pwd,
+                hostname:process.argv.hname,
+                port:process.argv.port,
+                dbname:'hasuradb'
+              }
+            }
+            this.liveDb=new LivePg(this.getConnectionString(), value||"ramu");
+
+            process.on('SIGINT', function() {
+                this.liveDb.cleanup(process.exit);
+              });
+    }
+
+    select(query,handler){
+        try{
+        this.liveDb.select(query).on('update',handler);
+        }catch(e){
+            return e;
+        }
+    }
+
+    close(){
+        this.liveDb.cleanup(process.exit);
+    }
+
+    
+}
+
+module.exports=LiveQuery;
--- microservices/real/app/src/server.js
+++ microservices/real/app/src/server.js
@@ -1,34 +1,7 @@
 var express = require('express');
+var LiveQuery=require('./liveQuery');
 var app = express();
-var fs = require('fs');
-var path = require('path');
-var LivePg = require('pg-live-select');
 
-
-var dbcred;
-
-if(!process.argv){
-dbcred={
-  username:process.env.POSTGRES_USERNAME,
-  password:process.env.POSTGRES_PASSWORD,
-  hostname:process.env.POSTGRES_HOSTNAME,
-  port:process.env.POSTGRES_PORT,
-  dbname:'hasuradb'
-}
-}
-else{
-  dbcred={
-    username:process.env.POSTGRES_USERNAME,
-    password:process.env.POSTGRES_PASSWORD,
-    hostname:process.env.POSTGRES_HOSTNAME,
-    port:process.env.POSTGRES_PORT,
-    dbname:'hasuradb'
-  }
-}
-
-
-
-console.log(JSON.stringify(dbcred));
 //your routes here
 app.get('/', function (req, res) {
     res.send("Hello World!");
@@ -39,29 +12,13 @@ app.listen(8080, function () {
 });
 
 
-// Update this line with your username/password/host/database
-var CONN_STR = 'postgres://'+dbcred.username+':'
-+dbcred.password
-+'@'+dbcred.hostname+':'+dbcred.port+'/'+dbcred.dbname;
-// Load the SELECT query from an external file
-var QUERY = "select * from ramu";
+liveQuery=new LiveQuery("first");
 
-// Initialize the live query processor
-var liveDb = new LivePg(CONN_STR, 'mytest');
-
-// Create a live select instance
 
 try{
-liveDb.select(QUERY)
-  .on('update', function(diff, data) {
-    // Handle the changes here...
-    console.log(diff, data);
-  });
-}catch(e){
-console.log(JSON.stringify(e));
-}
-
-// On Ctrl+C, remove triggers and exit
-process.on('SIGINT', function() {
-  liveDb.cleanup(process.exit);
+liveQuery.select('select * from user',function(diff,data){
+    console.log(JSON.stringify(diff)+"  data: "+JSON.stringify(data));
 });
+}catch(e){
+  console.log(JSON.stringify(e));
+}
