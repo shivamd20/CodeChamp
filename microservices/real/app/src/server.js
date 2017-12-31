e753a3a222d890ebb20c0e832e64d583733f12b9
=shivam dwivedi <=shivamd20@gmail.com>
2017-12-31 15:53:53 +0530

    live query

--- microservices/real/app/src/server.js
+++ microservices/real/app/src/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var path = require('path');
+var LivePg = require('pg-live-select');
 
 //your routes here
 app.get('/', function (req, res) {
@@ -8,4 +11,25 @@ app.get('/', function (req, res) {
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
+});
+
+
+// Update this line with your username/password/host/database
+var CONN_STR = 'postgres://shivam:shivamd20@127.0.0.1/postgres';
+// Load the SELECT query from an external file
+var QUERY = "select * from ramu";
+
+// Initialize the live query processor
+var liveDb = new LivePg(CONN_STR, 'mytest');
+
+// Create a live select instance
+liveDb.select(QUERY)
+  .on('update', function(diff, data) {
+    // Handle the changes here...
+    console.log(diff, data);
+  });
+
+// On Ctrl+C, remove triggers and exit
+process.on('SIGINT', function() {
+  liveDb.cleanup(process.exit);
 });
