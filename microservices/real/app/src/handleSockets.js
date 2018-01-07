8e355cd25dfe7b64232601e377dc2b3a4737cfb4
shivam <shivamd20@gmail.com>
2018-01-08 00:59:21 +0530

    added DATA_HOSTNAME VARIABLE

--- microservices/real/app/src/handleSockets.js
+++ microservices/real/app/src/handleSockets.js
@@ -7,7 +7,9 @@ function queryData(query, token) {
    // var url = "https://data.circadian84.hasura-app.io/v1/query";
 
 
-    var url = "http://data.hasura/v1/query";
+  
+
+    var url = "http://"+ process.env.DATA_HOSTNAME;+"/v1/query";
 
 
     var requestOptions = {
