fea251eeffb7df17a4775d25fb40c7026c0f7bfd
shivam <shivamd20@gmail.com>
2018-02-03 22:07:04 +0530

    onCn bug fixed

--- microservices/real/app/src/handleSockets.js
+++ microservices/real/app/src/handleSockets.js
@@ -53,10 +53,205 @@ class HandleSocket {
         return query(options);
 
     }
+    
+    onConnection = (socket) => {
 
+        socket.selectMap = new Map();
+
+        socket.on('proxyrq', (options, fn)=>{
+
+                query(options).then((response)=>{
+                    isFunction(fn)
+                    fn({
+                        "status": 'ok',
+                        'data': response.data,
+                        "headers" : response.headers,
+                        "statusCode" : response.status,
+                        statusText : response.statusText
+                    });
+                }).catch((err) => {
+                    isFunction(fn)
+                    fn(
+                        {
+                            'status': 'error',
+                            'error': err.response ? err.response.data : err.toString(),
+                            "headers" : err.response ? err.response.headers : null,
+                            "statusCode" :  err.response ? err.response.status : null,
+                            statusText : err.response ? err.response.statusText : null
+                        });
+
+                    //     console.log(err);
+
+                });
+
+        });
+
+        socket.on('queryauth', (query,path, fn) => {
+
+            this.queryAuth(query,path, socket.token)
+                .then(response => {
+
+                    //  console.log(response);
+
+                    isFunction(fn)
+                    fn({
+                        "status": 'ok',
+                        'data': response.data
+                    });
+                })
+                .catch((err) => {
+                    isFunction(fn)
+                    fn(
+                        {
+                            'status': 'error',
+                            'error': err.response.data
+                        });
+
+                    //     console.log(err);
+
+                });
+
+              
+        });
+
+        socket.on('querydata', (query, fn) => {
+
+            this.queryData(query, socket.token)
+                .then(response => {
+
+                    //  console.log(response);
+
+                    isFunction(fn)
+                    fn({
+                        "status": 'ok',
+                        'data': response.data,
+                        "headers" : response.headers,
+                        "statusCode" : response.status,
+                        statusText : response.statusText
+                    });
+                })
+                .catch((err) => {
+                    isFunction(fn)
+                    fn(
+                        {
+                            'status': 'error',
+                            'error': err.response.data,
+                            "headers" : err.response ? err.response.headers : null,
+                            "statusCode" :  err.response ? err.response.status : null,
+                            statusText : err.response ? err.response.statusText : null
+                        });
+
+                    //     console.log(err);
+
+                });
+
+
+        });
+
+        socket.on("settoken", (data, fn) => {
+
+            socket.token = data;
+            isFunction(fn)
+            fn({
+                status: 'okay',
+                message: 'auth token set'
+            });
+
+        })
+
+        socket.on('subscribe', (data, key, fn) => {
+
+            //   var  includediff = what.diff?true:false,
+            //     includedata = what.data?true:false;
+
+            // console.log('key:  ' + key);
+
+            if (socket.selectMap.get(key)) {
+                socket.selectMap.get(key).stop();
+            }
+
+            this.queryData(data, socket.token)
+
+                .then((result) => {
+
+                    //   console.log('permission granted')
+                    //   console.log( JSON.stringify(result.data));
+
+                    var sql = convertToString(data);
+
+                    socket.selectMap.set(key, liveQuery.select(sql.query, sql.values, (diff, data) => {
+
+                        socket.emit('datachange' + key, data);
+
+                        //    console.log("key: " + key + "  diff:  " + JSON.stringify(diff) + "  data: " + JSON.stringify(data));
+                    }
+                        , (e) => {
+
+                            console.log(e);
+                            isFunction(fn)
+                            fn({
+                                error: e.toString(),
+
+                                cause: '2'
+                            })
+                        }))
+                }
+                )
+                .catch((err) => {
+                    //    fn(err.toString());
+
+                    var errstr;
+
+                    if (err.response) {
+                        errstr = err.response.data
+                    } else {
+                        errstr = err.toString();
+                    }
+
+                    isFunction(fn)
+                    fn({
+                        error: errstr,
+
+                        cause: '1'
+
+                    });
+
+                });
+                
+        });
+
+
+        socket.on('unsubscribe', (key, fn) => {
+
+            var lq = socket.selectMap.get(key);
+
+            if (lq) {
+                lq.stop();
+                lq = undefined;
+                socket.selectMap.push(key, undefined);
+
+                isFunction(fn)
+                fn(key, {
+                    status: 'unsubscribed'
+                });
+            }
+            else
+
+                isFunction(fn)
+            fn(key, {
+                status: 'key not subscribed'
+            });
+
+        });
+
+        if(isFunction(this.onCn))
+        this.onCn(socket);
+    }
  
 
     constructor(server, config, onCn) {
+
+        this.onCn = onCn;
 
         if (!config) config = {};
 
@@ -69,186 +264,7 @@ class HandleSocket {
 
         //this.io.set('origins', '*');
 
-        this.onConnection = (socket) => {
-
-            socket.selectMap = new Map();
-
-            socket.on('proxyrq', (options, fn)=>{
-
-                    query(options).then((response)=>{
-                        isFunction(fn)
-                        fn({
-                            "status": 'ok',
-                            'data': response.data
-                        });
-                    }).catch((err) => {
-                        isFunction(fn)
-                        fn(
-                            {
-                                'status': 'error',
-                                'error': err.response.data
-                            });
-
-                        //     console.log(err);
-
-                    });
-
-            });
-
-            socket.on('queryauth', (query,path, fn) => {
-
-                this.queryAuth(query,path, socket.token)
-                    .then(response => {
-
-                        //  console.log(response);
-
-                        isFunction(fn)
-                        fn({
-                            "status": 'ok',
-                            'data': response.data
-                        });
-                    })
-                    .catch((err) => {
-                        isFunction(fn)
-                        fn(
-                            {
-                                'status': 'error',
-                                'error': err.response.data
-                            });
-
-                        //     console.log(err);
-
-                    });
-
-                    if(isFunction(onCn))
-                    onCn(socket);
-                  
-            });
-
-            socket.on('querydata', (query, fn) => {
-
-                this.queryData(query, socket.token)
-                    .then(response => {
-
-                        //  console.log(response);
-
-                        isFunction(fn)
-                        fn({
-                            "status": 'ok',
-                            'data': response.data
-                        });
-                    })
-                    .catch((err) => {
-                        isFunction(fn)
-                        fn(
-                            {
-                                'status': 'error',
-                                'error': err.response.data
-                            });
-
-                        //     console.log(err);
-
-                    });
-
-
-            });
-
-            socket.on("settoken", (data, fn) => {
-
-                socket.token = data;
-                isFunction(fn)
-                fn({
-                    status: 'okay',
-                    message: 'auth token set'
-                });
-
-            })
-
-            socket.on('subscribe', (data, key, fn) => {
-
-                //   var  includediff = what.diff?true:false,
-                //     includedata = what.data?true:false;
-
-                // console.log('key:  ' + key);
-
-                if (socket.selectMap.get(key)) {
-                    socket.selectMap.get(key).stop();
-                }
-
-                this.queryData(data, socket.token)
-
-                    .then((result) => {
-
-                        //   console.log('permission granted')
-                        //   console.log( JSON.stringify(result.data));
-
-                        var sql = convertToString(data);
-
-                        socket.selectMap.set(key, liveQuery.select(sql.query, sql.values, (diff, data) => {
-
-                            socket.emit('datachange' + key, data);
-
-                            //    console.log("key: " + key + "  diff:  " + JSON.stringify(diff) + "  data: " + JSON.stringify(data));
-                        }
-                            , (e) => {
-
-                                console.log(e);
-                                isFunction(fn)
-                                fn({
-                                    error: e.toString(),
-
-                                    cause: '2'
-                                })
-                            }))
-                    }
-                    )
-                    .catch((err) => {
-                        //    fn(err.toString());
-
-                        var errstr;
-
-                        if (err.response) {
-                            errstr = err.response.data
-                        } else {
-                            errstr = err.toString();
-                        }
-
-                        isFunction(fn)
-                        fn({
-                            error: errstr,
-
-                            cause: '1'
-
-                        });
-
-                    });
-                    
-            });
-
-
-            socket.on('unsubscribe', (key, fn) => {
-
-                var lq = socket.selectMap.get(key);
-
-                if (lq) {
-                    lq.stop();
-                    lq = undefined;
-                    socket.selectMap.push(key, undefined);
-
-                    isFunction(fn)
-                    fn(key, {
-                        status: 'unsubscribed'
-                    });
-                }
-                else
-
-                    isFunction(fn)
-                fn(key, {
-                    status: 'key not subscribed'
-                });
-
-            });
-        }
+      
         this.io.on('connection', this.onConnection);
     }
 
